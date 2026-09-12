243c8818c2971f2cd49c6e4bb8320db5b38efdb2
sqsaay <ayeleth.quevedo@gmail.com>
2026-09-12 15:03:24 -0600

    first commit

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+/* const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect');
+const professionalRoutes = require('./routes/professional');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/professional', professionalRoutes);
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+}); */
+
+/* const express = require('express');
+const app   = express();
+app.get('/', (req, res) => {
+  res.send('Helloadfsd World!');
+});
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+}); */
